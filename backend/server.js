839b3de01af494dde6713cbeeaaa4dca64c3fd22
Roshan Patil <patilroshan9687@gmail.com>
2026-01-18 16:38:55 +0530

    import app.js

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
+import {app} from "./app.js"
 
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-  res.send('Hello from the backend server!');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
