cc1d4c4046b489288844d31bec235cb5533e1157
Roshan Patil <patilroshan9687@gmail.com>
2026-01-20 14:27:24 +0530

    write hashpassword method before save to the database

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
     {
@@ -26,5 +27,11 @@ const userSchema = new Schema(
         }
     },{timestamps: true}
 )
+
+// Hash password before saving to database
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+})  
 
 export const User = mongoose.model("User",userSchema) 
