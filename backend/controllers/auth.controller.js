e139831f386a1296c6d52a5ab370ab98c9235d6b
Roshan Patil <patilroshan9687@gmail.com>
2026-02-09 19:06:09 +0530

    refactor: improve error handling and code formatting in authentication controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -6,117 +6,115 @@ import bcrypt from "bcryptjs";
 import { generateTokens } from "../utils/generateTokens.js";
 import { cookieOptions } from "../utils/cookieOptions.js";
 
-
-//Todo : sign up with avatar 
+//Todo : sign up with avatar
 const signUp = asyncHandler(async (req, res) => {
+  const { name, username, email, password } = req.body;
 
-    const { name, username, email, password } = req.body;
+  if (!name || !username || !email || !password) {
+    throw new ApiError(400, "All fields are required");
+  }
 
-    if (!name || !username || !email || !password) {
-        throw new ApiError(400, "All fields are required");
-    }
+  //email validation regex
 
-    //email validation regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+    throw new ApiError(400, "Invalid email format");
+  }
 
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!emailRegex.test(email)) {
-        throw new ApiError(400, "Invalid email format");
-    }
+  // check if user already exists
 
-    // check if user already exists
+  const existingUser = await User.findOne({ $or: [{ email }, { username }] });
 
-    const existingUser = await User.findOne({ $or: [{ email }, { username }] })
+  if (existingUser) {
+    throw new ApiError(400, "User already exists.");
+  }
 
-    if (existingUser) {
-        throw new ApiError(400, "User already exists.")
-    }
+  // password validatoin regex
 
-    // password validatoin regex
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!passwordRegex.test(password)) {
+    throw new ApiError(
+      400,
+      "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.",
+    );
+  }
 
-    if (!passwordRegex.test(password)) {
-        throw new ApiError(400, "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.");
-    }
+  const user = await User.create({
+    name,
+    username,
+    email,
+    password,
+  });
 
-    const user = await User.create({
-        name, username, email, password,
-    })
+  if (!user) {
+    throw new ApiError(500, "User Signup failed. Please try again.");
+  }
 
-    if (!user) {
-        throw new ApiError(500, "User Signup failed. Please try again.")
-    }
-
-    res.status(201).json(
-        new ApiResponse(201, "User registered successfully", {
-            userId: user._id,
-            name: user.name,
-            username: user.username,
-            email: user.email
-        })
-    )
-})
+  res.status(201).json(
+    new ApiResponse(201, "User registered successfully", {
+      userId: user._id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+    }),
+  );
+});
 
 //login controller
 
 const login = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-    const { email, password } = req.body;
+  if (!email || !password) {
+    throw new ApiError(400, "Email and Password are required");
+  }
 
-    if (!email || !password) {
-        throw new ApiError(400, "Email and Password are required");
-    }
+  const user = await User.findOne({
+    email,
+  });
 
-    const user = await User.findOne({
-        email
-    })
+  if (!user) {
+    throw new ApiError(404, "User not found. Please sign up.");
+  }
 
-    if (!user) {
-        throw new ApiError(404, "User not found. Please sign up.")
-    }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid password. Please try again.");
+  }
 
-    if (!isPasswordValid) {
-        throw new ApiError(401, "Invalid password. Please try again.")
-    }
+  const { accessToken, refreshToken } = await generateTokens(user._id);
 
-    const { accessToken, refreshToken } = await generateTokens(user._id);
-    
-    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken",
+  );
 
-    const options = cookieOptions;
-    res
-        .status(200)
-        .cookie("refreshToken", refreshToken, options)
-        .cookie("accessToken", accessToken, options)
-        .json(
-            new ApiResponse(200, "Login successful",
-                {
-                    user: loggedInUser,
-                    tokens: { accessToken, refreshToken }
-                }
-            )
-        )
-})
+  const options = cookieOptions;
+  res
+    .status(200)
+    .cookie("refreshToken", refreshToken, options)
+    .cookie("accessToken", accessToken, options)
+    .json(
+      new ApiResponse(200, "Login successful", {
+        user: loggedInUser,
+      }),
+    );
+});
 
 //logout controller
 
 const logout = asyncHandler(async (req, res) => {
-    await User.findByIdAndUpdate(
-        req.user._id,
-        {
-            $unset: { refreshToken: 1 }
-        }
-    )
+  await User.findByIdAndUpdate(req.user._id, {
+    $unset: { refreshToken: 1 },
+  });
 
-    const options = cookieOptions;
-    res
-        .status(200)
-        .clearCookie("accessToken", options)
-        .clearCookie("refreshToken", options)
-        .json(
-            new ApiResponse(200, "Logout successful", {})
-        )
-})
-export { signUp, login, logout}
+  const options = cookieOptions;
+  res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, "Logout successful", {}));
+});
+export { signUp, login, logout };
