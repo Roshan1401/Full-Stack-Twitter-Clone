3316fb0d3fb95ccbef190209707dd7fdefffd93c
Roshan Patil <patilroshan9687@gmail.com>
2026-02-14 16:01:01 +0530

    feat: add loading state to Home component; display loading spinner while fetching posts

--- frontend/src/components/pages/Home/Home.jsx
+++ frontend/src/components/pages/Home/Home.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "../../../Redux/posts/postSlice.js";
 
 function Home() {
+  const [loading, setLoading] = useState(true);
   const posts = useSelector((state) => state.posts.posts);
   const dispatch = useDispatch();
 
@@ -31,6 +32,8 @@ function Home() {
       }
     } catch (error) {
       console.error("Error fetching posts:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -42,12 +45,29 @@ function Home() {
         </div>
         <div className="posts-container">
           <AddPost />
-          <div className="posts-list">
-            {/* Posts will be rendered here */}
-            {posts.map((post) => (
-              <Post key={post._id} post={post} />
-            ))}
-          </div>
+
+          {loading ? (
+            <div className="flex min-h-[60vh] flex-col items-center justify-center gap-3">
+              <svg
+                className="h-7 w-7 animate-spin"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+                aria-label="Loading"
+                role="img"
+              >
+                <path
+                  fill="rgba(116, 192, 252, 1)"
+                  d="M222.7 32.1c5 16.9-4.6 34.8-21.5 39.8-79.3 23.6-137.1 97.1-137.1 184.1 0 106 86 192 192 192s192-86 192-192c0-86.9-57.8-160.4-137.1-184.1-16.9-5-26.6-22.9-21.5-39.8s22.9-26.6 39.8-21.5C434.9 42.1 512 140 512 256 512 397.4 397.4 512 256 512S0 397.4 0 256c0-116 77.1-213.9 182.9-245.4 16.9-5 34.8 4.6 39.8 21.5z"
+                />
+              </svg>
+            </div>
+          ) : (
+            <div className="posts-list">
+              {posts.map((post) => (
+                <Post key={post._id} post={post} />
+              ))}
+            </div>
+          )}
         </div>
       </div>
     </div>
