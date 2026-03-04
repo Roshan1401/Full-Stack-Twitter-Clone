8c2a52af75e5d8e344d4e612a5563ce7c48db06e
Roshan Patil <patilroshan9687@gmail.com>
2026-03-04 18:51:26 +0530

    fix: improve code formatting and ensure consistent use of semicolons in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import {app} from "./app.js"
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import { app } from "./app.js";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({
-  path: "./.env"
+  path: "./.env",
 });
 
-const PORT = process.env.PORT || 5000
+console.log("Environment variables loaded:", process.env.PORT);
+const PORT = process.env.PORT || 5000;
 
-; (async () => {
+(async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/X-Clone`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/X-Clone`);
     console.log("Connected to MongoDB");
-    app.listen(5000, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
-})()
+})();
