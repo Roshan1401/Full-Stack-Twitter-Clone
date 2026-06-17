5088c720bc7f482771c06958a43aa6648ec3093c
Roshan Patil <patilroshan9687@gmail.com>
2026-06-17 14:27:50 +0530

    fix: move sizeClasses definition outside of UserAvatar component for better performance

--- frontend/src/components/common/UserAvatar.jsx
+++ frontend/src/components/common/UserAvatar.jsx
@@ -1,12 +1,11 @@
 import Avatar from "../Avatar/Avatar";
 
+const sizeClasses = {
+  small: "h-8 w-8",
+  default: "h-12 w-12",
+  large: "h-24 w-24",
+};
 function UserAvatar({ user, size = "default" }) {
-  const sizeClasses = {
-    small: "h-8 w-8",
-    default: "h-12 w-12",
-    large: "h-24 w-24",
-  };
-
   return (
     <Avatar
       imgUrl={user?.avatar?.url || "/userLogo1.jpg"}
