6ff91bc33d254e1991b93e65f2ed63bf50deae8a
Roshan Patil <patilroshan9687@gmail.com>
2026-07-27 23:43:47 +0530

    fix: Update CORS origin to use the correct production URL

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://full-stack-twitter-clone-beige.vercel.app",
-    ],
+    origin: ["http://localhost:5173", "https://devtalkk.vercel.app"],
     credentials: true,
   }),
 );
